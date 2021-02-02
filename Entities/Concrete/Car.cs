74e6f8ca3f9ad4d59eb7cb355642441dc066e706
Burak Can Kaya <burak.kaya24@ogr.sakarya.edu.tr>
2021-02-02 20:21:13 +0300

    Proje dosyası ekle.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public int BrandID { get; set; }
+        public int ColorID { get; set; }
+        public int ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public int MyProperty { get; set; }
+        public string Description { get; set; }
+
+    }
+}
